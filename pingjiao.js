5653d609f23d8b40677c3f11cd967b862588cf10
Han <hansnow2012@gmail.com>
2016-01-04 19:30:20 +0800

    add some comments

--- pingjiao.js
+++ pingjiao.js
@@ -1,8 +1,8 @@
+// 评语，urlencode(s.encode('gbk'))
 var comment = '%BD%B2%BF%CE%C8%CF%D5%E6';
+// task_array里包含了所有待评课程的信息
 var task_array = new Array();
 var el_array = window.frames['bottomFrame'].frames['mainFrame'].document.querySelectorAll('[title=评估]');
-// var info_table = document.querySelectorAll('[title=评估]')[0].getAttribute('name');
-// console.log(window.frames['bottomFrame'].frames['mainFrame'].document.documentElement.innerHTML);
 for (var i = el_array.length - 1; i >= 0; i--) {
     info = el_array[i].getAttribute('name').split('#@');
     var task = {
@@ -16,8 +16,10 @@ for (var i = el_array.length - 1; i >= 0; i--) {
     task_array.push(task);
 };
 
+// 手动发xhr
 var xmlhttp;
 var task;
+// stop变量用来控制xhr的发送数量，以防连续不断的发送请求，不知道有没有更优雅的解决方案
 var stop = task_array.length * 2;
 if( window.XMLHttpRequest ){
     xmlhttp = new XMLHttpRequest();
@@ -28,14 +30,14 @@ xmlhttp.onreadystatechange = function(){
     if( xmlhttp.readyState == 4 && xmlhttp.status == 200 ){
         if( stop !== 0 ){
             if ( stop%2 === 0 ){
-                //预备post
+                // 预备post
                 task = task_array.pop()
                 xmlhttp.open("POST","jxpgXsAction.do",true);
                 xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
                 xmlhttp.send("wjbm="+ task.wjbm +"&bpr="+ task.bpr +"&pgnr="+ task.pgnr +"&oper=wjShow&wjmc="+ task.wjmc +"&bprm="+ task.bprm +"&pgnrm="+ task.pgnrm +"&pageSize=20&page=1&currentPage=1&pageNo=");
 
             }else{
-                //评教post
+                // 评教post
                 xmlhttp.open("POST","jxpgXsAction.do?oper=wjpg",true);
                 xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
                 xmlhttp.send("wjbm="+ task.wjbm +"&bpr="+ task.bpr +"&pgnr="+ task.pgnr +"&0000000021=10_0.95&0000000022=10_0.95&0000000023=5_0.95&0000000024=20_0.95&0000000025=10_0.95&0000000026=5_0.95&0000000027=5_0.95&0000000028=20_0.95&0000000029=10_0.95&0000000030=5_0.95&zgpj="+comment);
@@ -43,10 +45,10 @@ xmlhttp.onreadystatechange = function(){
             }
             stop--;
         }else{
-            alert('已完成');
+            alert('已完成，请刷新页面查看效果');
         }
     }
 }
-
+// 这个一个没用的请求，用来使`stop`的逻辑更简单
 xmlhttp.open("GET","jxpgXsAction.do?oper=listWj",true);
 xmlhttp.send();
